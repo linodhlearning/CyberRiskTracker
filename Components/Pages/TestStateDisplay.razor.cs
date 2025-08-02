18131a5cb18034b2a01456827bbe7accbf1ec6c2
linodhlearning <linodhcalendar@gmail.com>
2025-08-02 22:13:40 +1000

    state added and displayed

---
+++ Components/Pages/TestStateDisplay.razor.cs
@@ -0,0 +1,21 @@
+﻿using CyberRiskTracker.State;
+using Microsoft.AspNetCore.Components;
+
+namespace CyberRiskTracker.Components.Pages
+{
+    public partial class TestStateDisplay
+    {
+        [Inject]
+        public required TestApplicationState AppState { get; set; }
+        private void UpdateTestString()
+        {
+            AppState.TestInt += 10;
+            AppState.TestString = "Updated at " + DateTime.Now.ToLongTimeString();
+        }
+
+        protected override void OnInitialized()
+        {
+            AppState.TestInt += 1;
+        }
+    }
+}
